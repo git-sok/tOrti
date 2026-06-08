040b0b53da2451cc81c80b00196b41ce45d6ace7
sok_ <blinetpamyti@gmail.com>
2026-06-08 18:25:31 +0300

    Добавил один ряд карточек через JS

---
+++ script.js
@@ -0,0 +1,35 @@
+const topleft = document.querySelector('.katalog__products-top-cards-left');
+
+ const img = topleft.querySelector('img');
+ const title = topleft.querySelector('.global__h4-title');
+ const description = topleft.querySelector('.global__description-card');
+
+ img.src = 'imgs/img.png';
+ title.textContent = 'Кремовый замок';
+ description.textContent = 'Нежный крем любого цвета на выбор, вафельная основа';
+
+
+
+ const topcenter = document.querySelector('.katalog__products-top-cards-center');
+
+ const img2 = topcenter.querySelector('img');
+ const title2 = topcenter.querySelector('.global__h4-title');
+ const description2 = topcenter.querySelector('.global__description-card');
+
+ img2.src = 'imgs/img-1.png';
+ title2.textContent = 'Малиновый рай';
+ description2.textContent = 'Воздушный крем, темная основа и ягода малины';
+
+
+
+ const topright = document.querySelector('.katalog__products-top-cards-right');
+
+ const img3 = topright.querySelector('img');
+ const title3 = topright.querySelector('.global__h4-title');
+ const description3 = topright.querySelector('.global__description-card');
+
+ img3.src = 'imgs/img-2.png';
+ title3.textContent = 'Фейерверк';
+ description3.textContent = 'Разноцветные крем, с бисквитной основой';
+
+
