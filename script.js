028472951807f57b1f952b5e2fcd85b3783c18a7
sok_ <blinetpamyti@gmail.com>
2026-06-08 21:58:50 +0300

    Добавил все ряды карточек через JS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+            //TOP_CARDS
+
 const topleft = document.querySelector('.katalog__products-top-cards-left');
 
  const img = topleft.querySelector('img');
@@ -33,3 +35,78 @@ const topleft = document.querySelector('.katalog__products-top-cards-left');
  description3.textContent = 'Разноцветные крем, с бисквитной основой';
 
 
+                //CENTER_CARDS
+
+
+const centerleft = document.querySelector('.katalog__products-center-cards-left');
+
+ const img4 = centerleft.querySelector('img');
+ const title4 = centerleft.querySelector('.global__h4-title');
+ const description4 = centerleft.querySelector('.global__description-card');
+
+ img4.src = 'imgs/img-3.png';
+ title4.textContent = 'Шоколадный мир';
+ description4.textContent = 'Ореховая стружка, нежный крем и шоколадная основа';               
+
+
+
+ const centercenter = document.querySelector('.katalog__products-center-cards-center');
+
+ const img5 = centercenter.querySelector('img');
+ const title5 = centercenter.querySelector('.global__h4-title');
+ const description5 = centercenter.querySelector('.global__description-card');
+
+ img5.src = 'imgs/img-4.png';
+ title5.textContent = 'Слезы дракона';
+ description5.textContent = 'Нежный крем любого цвета на выбор, вафельная основа';               
+
+
+
+ const centerright = document.querySelector('.katalog__products-center-cards-right');
+
+ const img6 = centerright.querySelector('img');
+ const title6 = centerright.querySelector('.global__h4-title');
+ const description6 = centerright.querySelector('.global__description-card');
+
+ img6.src = 'imgs/img-5.png';
+ title6.textContent = 'Летняя фантазия';
+ description6.textContent = 'Украшения в форме сердец, для любимого человека';  
+ 
+ 
+                //BOTTON_CARDS
+
+
+
+const bottonleft = document.querySelector('.katalog__products-botton-cards-left');
+
+ const img7 = bottonleft.querySelector('img');
+ const title7 = bottonleft.querySelector('.global__h4-title');
+ const description7 = bottonleft.querySelector('.global__description-card');
+
+ img7.src = 'imgs/img-6.png';
+ title7.textContent = 'Мыс безумия';
+ description7.textContent = 'Разноцветная основа, стружка и нежный крем';
+ 
+ 
+
+ const bottoncenter = document.querySelector('.katalog__products-botton-cards-center');
+
+ const img8 = bottoncenter.querySelector('img');
+ const title8 = bottoncenter.querySelector('.global__h4-title');
+ const description8 = bottoncenter.querySelector('.global__description-card');
+
+ img8.src = 'imgs/img-7.png';
+ title8.textContent = 'Облачная сказка';
+ description8.textContent = 'Светлая основа, нежный крем со стружкой сверху';
+
+
+
+ const bottonright = document.querySelector('.katalog__products-botton-cards-right');
+
+ const img9 = bottonright.querySelector('img');
+ const title9 = bottonright.querySelector('.global__h4-title');
+ const description9 = bottonright.querySelector('.global__description-card');
+
+ img9.src = 'imgs/img-8.png';
+ title9.textContent = 'Темный рыцарь';
+ description9.textContent = 'Тёмная основа, нежный крем и вкусные шарики';
